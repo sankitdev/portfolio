0c7c569732426a571978b6a5868b5e15ff5f7e08
Ankit Singh <ankitsingh635101@gmail.com>
2024-09-22 18:12:04 +0530

    💼 Add skill section

---
+++ src/components/Skill.jsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import { FrontEnd, BackEnd, Technology } from "../Images";
+const Skill = () => {
+  const [activeTab, setActiveTab] = useState("FrontEnd");
+  const handleToggle = (tab) => {
+    setActiveTab(tab);
+  };
+  return (
+    <div className="text-center my-20">
+      <h1 className="text-4xl font-bold text-primary">Skills</h1>
+      <div
+        role="tablist"
+        className="tabs tabs-boxed mt-10 w-full sm:w-1/2 mx-auto "
+      >
+        <a
+          role="tab"
+          className={`tab ${activeTab === "FrontEnd" ? "tab-active" : ""}`}
+          onClick={() => handleToggle("FrontEnd")}
+        >
+          FrontEnd
+        </a>
+        <a
+          role="tab"
+          className={`tab ${activeTab === "BackEnd" ? "tab-active" : ""}`}
+          onClick={() => handleToggle("BackEnd")}
+        >
+          BackEnd
+        </a>
+        <a
+          role="tab"
+          className={`tab ${activeTab === "Technologies" ? "tab-active" : ""}`}
+          onClick={() => handleToggle("Technologies")}
+        >
+          Technologies
+        </a>
+      </div>
+      <div className="flex flex-wrap gap-5 justify-center items-center mt-10">
+        {activeTab === "FrontEnd" &&
+          FrontEnd.map((items, index) => (
+            <img
+              key={index}
+              src={items}
+              alt=""
+              className="hover:scale-95 btn w-28 h-28 object-contain"
+            />
+          ))}
+      </div>
+      <div className="flex flex-wrap gap-5 justify-center items-center">
+        {activeTab === "BackEnd" &&
+          BackEnd.map((items, index) => (
+            <img
+              key={index}
+              src={items}
+              alt=""
+              className="hover:scale-95 btn object-contain w-28 h-28"
+            />
+          ))}
+      </div>
+      <div className="flex flex-wrap gap-5 justify-center items-center">
+        {activeTab === "Technologies" &&
+          Technology.map((items, index) => (
+            <img
+              key={index}
+              src={items}
+              alt=""
+              className="hover:scale-95 btn object-contain w-28 h-28"
+            />
+          ))}
+      </div>
+    </div>
+  );
+};
+export default Skill;
