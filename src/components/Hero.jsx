725cfd9c7e538f6e5ac01924e1f45992e6297f15
Ankit Singh <ankitsingh635101@gmail.com>
2024-09-22 18:15:14 +0530

    Fix the UI and Add TyperWriter Effect

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,27 @@
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { about } from "../PortfolioData";
+import Typewriter from "typewriter-effect";
+
 // import test from "../assets/hero.jpg";
 const Hero = () => {
   const { name, role, description, resume, social } = about;
   return (
-    <div className="hero h-[calc(100vh-4rem)] sm:h-[calc(100vh-6rem)]">
+    <div className="hero h-[calc(100vh-7rem)] sm:h-[calc(100vh-6rem)]">
       <div className="hero-content text-center flex-col">
         <div>
           <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold py-2">
             Hii, I am <span className="text-primary">{name}</span>{" "}
             <span className="wave">👋</span>
           </h1>
-          <h2 className="text-2xl sm:text-3xl pt-6 font-semibold">{role}</h2>
+          <h2 className="text-2xl sm:text-3xl pt-6 font-semibold">
+            <Typewriter
+              options={{
+                strings: role,
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </h2>
           <p className="py-6 md:w-4/5 mx-auto sm:text-xl">{description}</p>
           <div className="flex justify-center items-center">
             <button className="btn btn-outline btn-primary px-5">
