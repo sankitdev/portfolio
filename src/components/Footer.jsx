174df8386cea32b14cc0ab943d32ab76fad4a194
Ankit Singh <ankitsingh635101@gmail.com>
2024-09-22 18:10:08 +0530

    Add Footer Section

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="footer footer-center text-base-content  py-5">
+      <aside>
+        <p>Copyright © {new Date().getFullYear()} - sankitdev</p>
+      </aside>
+    </footer>
+  );
+};
+
+export default Footer;
