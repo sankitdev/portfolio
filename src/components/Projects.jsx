b1f927a2e012943d1adad1f700cdcd6c79467806
Ankit Singh <ankitsingh635101@gmail.com>
2024-09-22 18:10:40 +0530

    Add more props for Project Data

---
+++ src/components/Projects.jsx
@@ -0,0 +1,24 @@
+import ProjectCards from "./ProjectCards";
+import { projects } from "../PortfolioData";
+const Project = () => {
+  return (
+    <>
+      <h1 className="text-center text-4xl text-primary font-semibold">
+        PROJECTS
+      </h1>
+      <div className="flex justify-center items-center flex-wrap mt-2">
+        {projects.map((items) => (
+          <ProjectCards
+            key={items.name}
+            name={items.name}
+            image={items.image}
+            techStack={items.stack}
+            description={items.description}
+            github={items.sourceCode}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+export default Project;
