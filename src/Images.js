50771a66413675b652113e2d27d98086144380b2
Ankit Singh <ankitsingh635101@gmail.com>
2024-09-22 14:46:26 +0530

    Add images array for use in Skills section

---
+++ src/Images.js
@@ -0,0 +1,15 @@
+// images.js
+import html from "./assets/html.svg";
+import css from "./assets/css.svg";
+import js from "./assets/js.svg";
+import ts from "./assets/ts.svg";
+import react from "./assets/react.svg";
+import next from "./assets/next.svg";
+import tailwind from "./assets/tailwind.svg";
+import node from "./assets/node js.svg";
+import express from "./assets/express.svg";
+import mongo from "./assets/mongo.svg";
+
+const Images = [html, css, js, ts, react, next, tailwind, node, express, mongo];
+
+export default Images;
