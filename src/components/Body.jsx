cf96ade28ad5192bf354657d7490c80280a9bcd4
Ankit Singh <ankitsingh635101@gmail.com>
2024-09-22 10:11:46 +0530

    Wrap all child components inside Body component

---
+++ src/components/Body.jsx
@@ -0,0 +1,15 @@
+import Hero from "./Hero";
+import Project from "./Project";
+import Skill from "./Skill";
+
+const Body = () => {
+  return (
+    <>
+      <Hero />
+      <Project />
+      <Skill />
+    </>
+  );
+};
+
+export default Body;
